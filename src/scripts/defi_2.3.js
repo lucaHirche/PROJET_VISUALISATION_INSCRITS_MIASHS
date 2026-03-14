7602a610cb4cea75eef35bc4fb306254d81ace72
chanwinharold <chanwinharold@gmail.com>
2026-03-14 23:35:58 +0100

    Last Code Review

--- src/scripts/defi_2.3.js
+++ src/scripts/defi_2.3.js
@@ -1,57 +1,55 @@
-
 const labels = [
-'2021-22',
-'2022-23',
-'2023-24',
-'2024-25'
+    '2021-22',
+    '2022-23',
+    '2023-24',
+    '2024-25'
 ];
 
 
 const data = {
-labels: labels,
-datasets: [
-{
-label: 'Les inscrits hommes à l\'UT2J en L2 MIASHS',
-data: [90, 76, 85, 85],
-borderColor: 'turquoise', 
-backgroundColor: 'rgba(0, 150, 136, 0.2)',
-borderWidth: 2,
-tension: 0.1 
-},
-{
-label: 'Les inscrites femmes à l\'UT2J en L2 MIASHS',
-data: [58, 44, 50, 45],
-borderColor: 'rgb(76, 175, 80)', 
-backgroundColor: 'rgba(76, 175, 80, 0.2)',
-borderWidth: 2,
-tension: 0.1
-}
-]
+    labels: labels,
+    datasets: [
+        {
+            label: 'Les inscrits hommes à l\'UT2J en L2 MIASHS',
+            data: [90, 76, 85, 85],
+            borderColor: 'turquoise',
+            backgroundColor: 'rgba(0, 150, 136, 0.2)',
+            borderWidth: 2,
+            tension: 0.1
+        },
+        {
+            label: 'Les inscrites femmes à l\'UT2J en L2 MIASHS',
+            data: [58, 44, 50, 45],
+            borderColor: 'rgb(76, 175, 80)',
+            backgroundColor: 'rgba(76, 175, 80, 0.2)',
+            borderWidth: 2,
+            tension: 0.1
+        }
+    ]
 };
 
 
 const config = {
-type: 'line', 
-data: data,
-options: {
-responsive: true,
-scales: {
-y: {
-
-min: 40,
-max: 90
-}
-},
-plugins: {
-legend: {
-position: 'top',
-}
-}
-}
+    type: 'line',
+    data: data,
+    options: {
+        responsive: true,
+        scales: {
+            y: {
+                min: 40,
+                max: 90
+            }
+        },
+        plugins: {
+            legend: {
+                position: 'top',
+            }
+        }
+    }
 };
 
 
 const myChart = new Chart(
-document.getElementById('monGraphiqueGenre'),
-config
+    document.getElementById('MonGraphe'),
+    config
 );
